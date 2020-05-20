db4fc4cb70c01068b9c03ab50a7a5a1fe5254921
christinapbui <christina.bui2@gmail.com>
2020-05-20 16:08:13 +0700

    version 2 with card

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let newsList = []
 let page = 1
+let newsSources = []
 const apiKey = "d46ade4a93754907b28028ce14102343" // used const so it won't be changed
 const loadNews = async() => { // using async to call data, will take long time // will call the news
    let url = `http://newsapi.org/v2/everything?q=vietnam&pagecount=${page}&apiKey=${apiKey}` // we need to call URL here 
@@ -15,7 +16,8 @@ const loadNews = async() => { // using async to call data, will take long time /
 // show the data to users--the UI
 const render = (list) => { // list is array
     console.log("render list",list) // list is array
-    let newsHtml = list.map(item => `<div id="news">
+    let newsHtml = list.map(item => `<div id="newsArea" class="card"> 
+    <div id="news">
     <div id="imgArea">
     <img src="${item.urlToImage}" width="200" />
     </div>
@@ -24,6 +26,7 @@ const render = (list) => { // list is array
         <div id="source">${item.source.name}</div>
         <div id="readMore"><a href="${item.url}">Read more...</a></div>
         <div id="publishedAt">${moment(item.publishedAt).startOf('day').fromNow()}</div>
+    </div>
     </div>
     </div>`).join('')
     document.getElementById("newsArea").innerHTML = newsHtml
@@ -42,3 +45,5 @@ let loadMoreNews = async() => {
     render(newsList)
     document.getElementById("numOfStories").innerHTML = `Number of Stories Displayed: ${newsList.length}`
 }
+
+
